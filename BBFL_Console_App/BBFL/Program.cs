325eff9fc149ccf7dddf06f7be440591179daef6
esb7cp <esb7cp@mail.missouri.edu>
2020-10-25 17:08:13 -0500

    added simple excel stuff

--- BBFL_Console_App/BBFL/Program.cs
+++ BBFL_Console_App/BBFL/Program.cs
@@ -9,9 +9,12 @@ namespace BBFL
             //Gets teams loaded into memory
             var teams = ProTeam.ReadInTeamsFromCSV(UtilityFunctions.pathTo_pro_teams);
 
-            //Gets two teams stats and load that into memory
-            teams[0].Stats = Statistics.ReadStatsFromCSV("1", teams[0]);
-            teams[1].Stats = Statistics.ReadStatsFromCSV("1", teams[1]);
+            //Gets teams stats and load that into memory
+            for (int i = 0; i < teams.Length; i++)
+            {
+                teams[i].Stats = Statistics.ReadStatsFromCSV("1", teams[i]);
+            }
+            
 
             float temp = Simulator.DoSimulation(teams[0], teams[1]);
             Console.WriteLine(teams[0].Team + " has a " + temp + "% chance to beat " + teams[1].Team);
